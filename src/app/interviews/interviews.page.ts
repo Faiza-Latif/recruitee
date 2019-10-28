980caa07d3b9482c5ae1f41f9e99096b4c1a4e4b
BrunoM24 <brunocostamatos.24@gmail.com>
2019-10-28 23:29:10 +0000

    Added new field dateTime to the interviews and filter the interviews on the main page to only show future interviews

--- src/app/interviews/interviews.page.ts
+++ src/app/interviews/interviews.page.ts
@@ -1,22 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { Interview } from './interview.model';
-import { InterviewsService } from './interviews.service';
+import {Component, OnInit} from '@angular/core';
+import {Interview} from './interview.model';
+import {InterviewsService} from './interviews.service';
 
 @Component({
-  selector: 'app-interviews',
-  templateUrl: './interviews.page.html',
-  styleUrls: ['./interviews.page.scss'],
+    selector: 'app-interviews',
+    templateUrl: './interviews.page.html',
+    styleUrls: ['./interviews.page.scss'],
 })
 export class InterviewsPage implements OnInit {
-  interviews: Interview[];
-  constructor(private interviewsService: InterviewsService) { }
+    interviews: Interview[];
 
-  ngOnInit() {
-    this.interviews = this.interviewsService.getAllInterviews();
-  }
+    constructor(private interviewsService: InterviewsService) {
+    }
 
-  createNewInterview() {
-    //TO DO : OPEN A ion-MODAL WITH THE HTML content on creation.txt (and remove that file)
-  }
+    ngOnInit() {
+        this.interviews = this.interviewsService.getAllInterviews().filter( interview => {
+            return interview.dateTime.toISOString() > new Date().toISOString();
+        });
+    }
+
+    createNewInterview() {
+        //TO DO : OPEN A ion-MODAL WITH THE HTML content on creation.txt (and remove that file)
+    }
 
 }
--- src/app/interviews/interviews.service.ts
+++ src/app/interviews/interviews.service.ts
@@ -1,57 +1,62 @@
-import { Injectable } from '@angular/core';
-import { Interview } from './interview.model';
+import {Injectable} from '@angular/core';
+import {Interview} from './interview.model';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class InterviewsService {
-  private interviews: Interview[] = [
-    {
-      companyName: 'Agap2',
-      jobDescription: 'Programmer',
-      status: 'firstInterview',
-      date: '2019-12-15',
-      time: '13:47',
-      alarm: false,
-      calendar: false,
-    },
-    {
-      companyName: 'Green',
-      jobDescription: 'Programmer',
-      status: 'firstInterview',
-      date: '2019-10-18',
-      time: '13:47',
-      alarm: false,
-      calendar: false,
-    },
-    {
-      companyName: 'Green',
-      jobDescription: 'Programmer',
-      status: 'secondInterview',
-      date: '2019-12-26',
-      time: '19:47',
-      alarm: false,
-      calendar: false,
+    private interviews: Interview[] = [
+        {
+            companyName: 'Agap2',
+            jobDescription: 'Programmer',
+            status: 'firstInterview',
+            date: '2019-12-15',
+            time: '13:47',
+            dateTime: new Date('2019-12-15 13:47'),
+            alarm: false,
+            calendar: false,
+        },
+        {
+            companyName: 'Green',
+            jobDescription: 'Programmer',
+            status: 'firstInterview',
+            date: '2019-10-18',
+            time: '13:47',
+            dateTime: new Date('2019-10-18 13:47'),
+            alarm: false,
+            calendar: false,
+        },
+        {
+            companyName: 'Green',
+            jobDescription: 'Programmer',
+            status: 'secondInterview',
+            date: '2019-12-26',
+            time: '19:47',
+            dateTime: new Date('2019-12-26 19:47'),
+            alarm: false,
+            calendar: false,
+        }
+    ];
+
+
+    constructor() {
     }
-  ];
 
- 
-  constructor() { }
 
-  getAllInterviews() {
-    return [...this.interviews];
-  }
+    getAllInterviews() {
+        return [...this.interviews];
+    }
 
-  getAllCurrentInterviews() {
-    //TO DO
-    //return interviews where date+time >= right now
-  }
+    getAllCurrentInterviews() {
+        // TO DO
+        // return interviews where date+time >= right now
+    }
 
-  /**
-   * Returns all the interviews related to a specific company 
-   * @param companyName
-   */
-  getInterviews(companyName: string) {
-    return [...this.interviews.filter(interview => interview.companyName === companyName)];
-  }
+    /**
+     * Returns all the interviews related to a specific company
+     * @param companyName
+     */
+    getInterviews(companyName: string) {
+        return [...this.interviews.filter(interview => interview.companyName === companyName)];
+    }
 }
